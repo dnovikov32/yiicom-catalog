715fb1f4b51e97d992f14b812f425ca7280fcc81
dn <dn@t1000.su>
2019-11-28 18:46:18 +0300

    fix(translate): translate file include removed to common/config

--- src/common/Module.php
+++ src/common/Module.php
@@ -7,14 +7,5 @@ use yii\i18n\PhpMessageSource;
 
 class Module extends \yiicom\common\Module
 {
-    public function init()
-    {
-        parent::init();
 
-        Yii::$app->i18n->translations['yiicom-catalog'] = [
-            'class' => PhpMessageSource::class,
-            'basePath' => '@yiicom/content/common/messages',
-            'sourceLanguage' => 'en-US',
-        ];
-    }
 }
