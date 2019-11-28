7ee129295303b8acc66f601ab5aeae5a070a5c83
dn <dn@t1000.su>
2019-11-28 16:53:07 +0300

    feat(translate): added translate file

--- src/common/Module.php
+++ src/common/Module.php
@@ -2,7 +2,19 @@
 
 namespace yiicom\catalog\common;
 
+use Yii;
+use yii\i18n\PhpMessageSource;
+
 class Module extends \yiicom\common\Module
 {
+    public function init()
+    {
+        parent::init();
 
+        Yii::$app->i18n->translations['yiicom-catalog'] = [
+            'class' => PhpMessageSource::class,
+            'basePath' => '@yiicom/content/common/messages',
+            'sourceLanguage' => 'en-US',
+        ];
+    }
 }
---
+++ src/common/messages/ru/yiicom-catalog.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'Catalog page' => 'Страница каталога',
+];
